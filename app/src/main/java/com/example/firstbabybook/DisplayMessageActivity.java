3530c9d672dfbf6e0f92eb735c00d68a54950ad9
Paola Panasiuk <pola@cachate.cl>
2020-03-06 15:14:54 -0300

    [develop]

--- app/src/main/java/com/example/firstbabybook/DisplayMessageActivity.java
+++ app/src/main/java/com/example/firstbabybook/DisplayMessageActivity.java
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_display_message)
 public class DisplayMessageActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-    }
 }
--- app/src/main/java/com/example/firstbabybook/MainActivity.java
+++ app/src/main/java/com/example/firstbabybook/MainActivity.java
@@ -1,31 +1,27 @@
 package com.example.firstbabybook;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
-
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.Click;
 
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.firstbabybook.MESSAGE";
-    @ViewById protected TextView  txtName = findViewById(R.id.txtName);
-    @ViewById protected TextView  txtLastName = findViewById(R.id.txtLastName);
-    @ViewById protected TextView  txtDateBorn = findViewById(R.id.txtDateBorn);
-    @ViewById protected TextView  txtPlaceBorn = findViewById(R.id.txtPlaceBorn);
-    @ViewById protected TextView  txtWeight = findViewById(R.id.txtWeight);
-    @ViewById protected TextView  txtSize = findViewById(R.id.txtSize);
+    @ViewById protected TextView  txtName;
+    @ViewById protected TextView  txtLastName;
+    @ViewById protected TextView  txtDateBorn;
+    @ViewById protected TextView  txtPlaceBorn;
+    @ViewById protected TextView  txtWeight;
+    @ViewById protected TextView  txtSize;
 
+    @Click(R.id.btnSave)
     public void saveFirstData(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText txtName = findViewById(R.id.txtName);
-        EditText txtLastName = findViewById(R.id.txtLastName);
-        String message = txtName.getText().toString();
-        message.concat(txtLastName.getText().toString());
+        String message = this.txtName.getText().toString();
+        message.concat(this.txtLastName.getText().toString());
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
