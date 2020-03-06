6cee5d1afd3c50f60b88fd537aba6144a174d8fe
Paola Panasiuk <pola@cachate.cl>
2020-03-05 21:57:51 -0300

    [develop]added annotation eactivity

--- app/src/main/java/com/example/firstbabybook/MainActivity.java
+++ app/src/main/java/com/example/firstbabybook/MainActivity.java
@@ -3,31 +3,29 @@ package com.example.firstbabybook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.firstbabybook.MESSAGE";
+    @ViewById protected TextView  txtName = findViewById(R.id.txtName);
+    @ViewById protected TextView  txtLastName = findViewById(R.id.txtLastName);
+    @ViewById protected TextView  txtDateBorn = findViewById(R.id.txtDateBorn);
+    @ViewById protected TextView  txtPlaceBorn = findViewById(R.id.txtPlaceBorn);
+    @ViewById protected TextView  txtWeight = findViewById(R.id.txtWeight);
+    @ViewById protected TextView  txtSize = findViewById(R.id.txtSize);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView txtName = findViewById(R.id.name);
-        TextView txtLastName = findViewById(R.id.lastname);
-        TextView txtDate_born = findViewById(R.id.date_born);
-        TextView txtPlace_born = findViewById(R.id.place_born);
-        TextView txtweight = findViewById(R.id.weight);
-        TextView txtSize = findViewById(R.id.size);
-        setContentView(R.layout.activity_main);
-    }
     public void saveFirstData(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText txtName = findViewById(R.id.name);
+        EditText txtName = findViewById(R.id.txtName);
+        EditText txtLastName = findViewById(R.id.txtLastName);
         String message = txtName.getText().toString();
+        message.concat(txtLastName.getText().toString());
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
