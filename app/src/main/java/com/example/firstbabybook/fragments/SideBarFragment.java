f5b5d0efdec72fbc4cf489a94a57b0e2e99249df
Paola Panasiuk <pola@cachate.cl>
2020-03-09 16:19:28 -0300

    [develop]adding fragment

---
+++ app/src/main/java/com/example/firstbabybook/fragments/SideBarFragment.java
@@ -0,0 +1,11 @@
+package com.example.firstbabybook.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.firstbabybook.R;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.fragment_sidebar)
+public class SideBarFragment extends Fragment {
+}
