c384f849ff7ba41c1d7ec41298e7e55ea2d17614
Paola Panasiuk <pola@cachate.cl>
2020-03-10 16:07:39 -0300

    [develop]adding dialog

--- app/src/main/java/com/example/firstbabybook/DialogTest.java
+++ app/src/main/java/com/example/firstbabybook/DialogTest.java
@@ -1,4 +1,18 @@
 package com.example.firstbabybook;
 
-public class DialogTest {
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+public class DialogTest  extends android.app.Dialog {
+
+    public DialogTest(@NonNull Context context) {
+        super(context);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_test);
+    }
 }
